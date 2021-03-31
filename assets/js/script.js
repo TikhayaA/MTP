049ab914b4e5e556d28f0a3502d7b93d20e28fb7
ATikha <tikhaya.a.v@gmail.com>
2021-03-31 16:44:29 +0300

    table

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,14 +48,56 @@ let mobileBg = document.getElementsByClassName('menu-mobile__bg');
 let mobileOpen = document.getElementsByClassName('menu-mobile__open');
 let mobileClose = document.getElementsByClassName('menu-mobile__close');
 
-    mobileOpen[0].addEventListener('click', () => {
-        menuMobile[0].classList.add('menu_active');
-        mobileBg[0].classList.add('menu-mobile__bg_active');
+    // mobileOpen[0].addEventListener('click', () => {
+    // //     menuMobile[0].classList.add('menu_active');
+    // //     mobileBg[0].classList.add('menu-mobile__bg_active');
+    // // });
+    // mobileClose[0].addEventListener('click', () => {
+    //     menuMobile[0].classList.remove('menu_active');
+    //     mobileBg[0].classList.remove('menu-mobile__bg_active');
+    // });
+
+const openSidebar = document.getElementsByClassName("open_sidebar");
+const tableFilter = document.getElementsByClassName("table_filter");
+
+if (openSidebar.length > 0) {
+    openSidebar[0].addEventListener('click', () => {
+        tableFilter[0].classList.toggle("active");
     });
-    mobileClose[0].addEventListener('click', () => {
-        menuMobile[0].classList.remove('menu_active');
-        mobileBg[0].classList.remove('menu-mobile__bg_active');
+}
+
+// select style 
+
+let selectSingle = document.querySelectorAll('.select');
+
+selectSingle.forEach((el, index) => {
+
+    let selectSingle_title = el.querySelector('.select__title');
+    let selectSingle_labels = el.querySelectorAll('.select__label');
+
+    // Toggle menu 
+    selectSingle_title.addEventListener('click', () => {
+
+        if (el.getAttribute('data-state') === 'active') {
+            el.setAttribute('data-state', '');
+        } else {
+            el.setAttribute('data-state', 'active');
+        }
     });
 
-
+    // Close when click to option 
+    for (let i = 0; i < selectSingle_labels.length; i++) {
+        selectSingle_labels[i].addEventListener('click', (evt) => {
+            selectSingle_title.textContent = evt.target.textContent;
+            el.setAttribute('data-state', '');
+        });
+    }
+    document.body.onclick = function(e) {
+        e = e || event;
+        target = e.target || e.srcElement;
+        if (target.className !== "select__title") {
+            el.setAttribute('data-state', '');
+        }
+    }
+});
 
